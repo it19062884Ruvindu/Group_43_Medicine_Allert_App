908d8df1650ae85913ae798863110aaa4aeb3a90
GayashaBudddhi <it19061580@my.sliit.lk>
2022-03-27 18:53:08 +0530

    firebase connected

---
+++ lib/pages/dosage/editdosage.dart
@@ -0,0 +1,85 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../main.dart';
+import 'package:flutter/material.dart';
+
+class editdosage extends StatefulWidget {
+  DocumentSnapshot docid;
+  editdosage({required this.docid});
+
+  @override
+  _editdosageState createState() => _editdosageState();
+}
+
+class _editdosageState extends State<editdosage> {
+  TextEditingController title = TextEditingController();
+  TextEditingController content = TextEditingController();
+
+  @override
+  void initState() {
+    title = TextEditingController(text: widget.docid.get('title'));
+    content = TextEditingController(text: widget.docid.get('content'));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              widget.docid.reference.update({
+                'title': title.text,
+                'content': content.text,
+              }).whenComplete(() {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => Home()));
+              });
+            },
+            child: Text("save"),
+          ),
+          MaterialButton(
+            onPressed: () {
+              widget.docid.reference.delete().whenComplete(() {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => Home()));
+              });
+            },
+            child: Text("delete"),
+          ),
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: TextField(
+                controller: title,
+                decoration: InputDecoration(
+                  hintText: 'title',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(border: Border.all()),
+                child: TextField(
+                  controller: content,
+                  expands: true,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    hintText: 'content',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
