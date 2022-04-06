8d205644cdfffd9783a0ac360ad338a3db93faae
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2022-04-06 08:57:27 +0530

    add appointment home page

--- lib/pages/main/home_page.dart
+++ lib/pages/main/home_page.dart
@@ -4,10 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:our_medicine_alert_43/pages/main/animation_welcome.dart';
 import 'package:our_medicine_alert_43/pages/main/login_page.dart';
 import '../../model/user_model.dart';
-import '../doctorvisit/add_channel.dart';
+import '../doctorvisit/channel_home.dart';
 import '../mymedicine/medicine_list.dart';
 import '../mymedicine/R_Beginning.dart';
-import '../doctorvisit/channel_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -197,7 +196,7 @@ class _HomePageState extends State<HomePage> {
                       ),
                       onPressed: () {
                         Navigator.push(context,
-                            MaterialPageRoute(builder: (_) => addchannel()));
+                            MaterialPageRoute(builder: (_) => ChannelHome()));
                       },
                       color: const Color(0xFF00838F),
                       child: Container(
